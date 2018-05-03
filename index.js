a698ecd2f79826dc97f4a9e09601e4190866424d
Unknown <fabianrk@gmail.com>
2018-05-03 16:20:20 +0300

    3

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-var votes= require ('./mdlVotes'),
-    express = require('express'),
-    app = express();
-    emitter = require('events');
-    
-
-
- 
-
+var votes= require ('./mdlVotes');
     var A = Array();
     var name = ['A','B','C'];
     for(var i=0;i<name.length;i++){
         A.push(new votes(name[i]));
         A[i].addCount();
         A[i].addCount();
-        console.log(A[i].getAllData());
+        A[0].addCount();
     }
+    A[2].reset();
+    A[1].reset();
+    A[0].Data();
+    A[1].Data();
+
+    app.listen(3000);
+    app.get('/',(req, res)=>{
+        res.json("A");
+    });
--- mdlVotes.js
+++ mdlVotes.js
@@ -1,40 +1,60 @@
 var events = require ('events');
+    express = require('express'),
+    app = express(),
+    A = Array();
 
-
+    module.exports=function printA(){
+        return A;
+    };
 class Votes extends events.EventEmitter{
+
     constructor(name){
         super();
         this.count=0;
         this.name=name;
+
+        this.on("Vote",(name)=> {
+            console.log(`Vote in ${name}`);
+            A.push(`Vote in ${name}`);
+                
+        });
+        this.on("Reset",(name)=> {
+            console.log(`Reset in ${name}`);
+            A.push(`Reset in ${name}`);
+        }); 
+        this.on("data",()=>{
+            console.log(`Data ${this.name}:`);
+            console.log(this.getAllData());
+            A.push(this.getAllData());
+        });
     }
     getName(){
         return this.name;
     }
     getAllData(){
-        var JsonObj = {
+        return{
             name:this.getName(),
             count:this.getCount()
-        };
-        return JsonObj;
+       };
     }
     setName(name){
         this.name=name;
     }
     getCount(){
         return this.count;
-
     }
     reset(){
         this.count=0;
-        this.emit('reset');
+        this.emit("Reset", this.name);
     }
     addCount(){
         this.count++;
-        this.emit('count');
+        this.emit("Vote",this.name);
     }
-    showall(){
-        this.emit('show');
+    Data(){
+        this.emit("data");
     }
+
 }
 
 module.exports = Votes;
